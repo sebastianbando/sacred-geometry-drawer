34795993812537b398d2c9c32cc92a68961b1f8a
Sebastian Bańdo <sydbeat@gmail.com>
2017-12-30 21:48:02 +0100

    rewrite for svg.js

--- script.js
+++ script.js
@@ -1,48 +1,20 @@
-var ctx = document.getElementById("myCanvas").getContext("2d");
+var draw = SVG('drawing').size(800, 600);
+//var rect = draw.rect(100, 100).attr({ fill: '#f06' });
 
-a = 20;
-h = (a*Math.pow(3, 1/2))/2;
+var a = 20;
+var h = (a*Math.pow(3, 1/2))/2;
+var x = 0;
+var y = 0;
 
-function drawIsoscelesTriangle1(ctx, x, y, a) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x+a/2, y-(a*Math.pow(3, 1/2))/2);
-    ctx.lineTo(x+a, y);
-    //ctx.lineTo(x, y);
-    ctx.closePath();
-    ctx.fillStyle = "black";
-    ctx.fill();
-    clx.click(function() {
-        this.fillStyle = "black";
-    });
-    //ctx.stroke();
-}
-function drawIsoscelesTriangle2(ctx, x, y, a) {
-    ctx.beginPath();
-    ctx.moveTo(x+a/2, y-(a*Math.pow(3, 1/2))/2);
-    ctx.lineTo(x+a, y);
-    ctx.lineTo(x+a/2+a, y-(a*Math.pow(3, 1/2))/2);
-    //ctx.lineTo(x, y);
-    ctx.closePath();
-    ctx.fillStyle = "black";
-    ctx.fill();
-    //ctx.stroke();
-}
-  
-for(i=0;i<40;i++){
-    for(j=0;j<40;j++){
-        drawIsoscelesTriangle1(ctx, j*a, i*h, a);
-    }
-}  
-for(i=0;i<40;i++){
-    for(j=0;j<40;j++){
-        drawIsoscelesTriangle2(ctx, j*a, i*h, a);
+for (i=0;i<20;i++) {
+    for (j=0;j<40;j++) {
+        var shape = draw.polygon(x+","+y+ " " +x+a/2+","+y+h+ " " +x+a+","+y)
+            .fill('black')
+            .move(j*a/2, i*h)
+        if (i%2==1 && j%2==1) {
+            shape.rotate(180) 
+        } else if (i%2==0 && j%2==0) {
+            shape.rotate(180) 
+        }
     }
 }
-
-
-
-// ==========================
-$(document).ready(function(){
-    $(".output").text(h);
-});
