6ddb39f2a3740bf44eef76bd1c8fb2bb21fd0d56
Sebastian Bańdo <sydbeat@gmail.com>
2017-12-30 22:36:47 +0100

    drawing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var draw = SVG('drawing').size(800, 600);
 //var rect = draw.rect(100, 100).attr({ fill: '#f06' });
 
-var a = 20;
+var a = 100;
 var h = (a*Math.pow(3, 1/2))/2;
 var x = 0;
 var y = 0;
@@ -9,8 +9,12 @@ var y = 0;
 for (i=0;i<20;i++) {
     for (j=0;j<40;j++) {
         var shape = draw.polygon(x+","+y+ " " +x+a/2+","+y+h+ " " +x+a+","+y)
-            .fill('black')
+            .fill('white')
             .move(j*a/2, i*h)
+        shape.click(function() {
+            this.fill({ color: 'black' })
+            //alert(this.fill())
+        });
         if (i%2==1 && j%2==1) {
             shape.rotate(180) 
         } else if (i%2==0 && j%2==0) {
